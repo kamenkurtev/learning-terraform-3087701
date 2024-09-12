9b0d94cc9a02128df1f59183428862c39e9a9961
kamenkurtev <kamenkurtev@gmail.com>
2024-09-12 17:15:16 +0300

    rename

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
 
-  name    = "blog_new"
+  name    = "blog"
 
   vpc_id              = data.aws_vpc.default.id
 
