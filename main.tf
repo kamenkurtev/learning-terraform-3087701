25584459bf175f9fcea2cf29e9b9a6718f095d2a
kamenkurtev <kamenkurtev@gmail.com>
2024-10-28 08:16:24 +0200

    fixes

--- main.tf
+++ main.tf
@@ -14,80 +14,47 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  name = "blog-alb"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  load_balancer_type = "application"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
- 
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-
-  security_groups = [module.blog_sg.security_group_id]
-
-
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
-  }
+  ]
 
-  listeners  = {
-    ex-http-tcp = {
+  http_tcp_listeners = [
+    {
       port               = 80
       protocol           = "HTTP"
-      target_group_index = 0  
+      target_group_index = 0
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
   }
 }
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
 
-  name    = "blog"
-
+  name                = "blog"
   vpc_id              = module.blog_vpc.vpc_id
-
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
