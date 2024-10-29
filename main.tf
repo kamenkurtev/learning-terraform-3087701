8f513d081119cfa22775e942885df212f2f3191e
kamenkurtev <kamenkurtev@gmail.com>
2024-10-29 07:11:48 +0200

    f03

--- main.tf
+++ main.tf
@@ -51,6 +51,14 @@ module "blog_alb" {
     }
   }
 
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
 
   tags = {
     Environment = "dev"
