b99e295aa570048287c22f4e6e8e43c15186c03b
kamenkurtev <kamenkurtev@gmail.com>
2024-10-29 07:10:18 +0200

    f02

--- main.tf
+++ main.tf
@@ -48,12 +48,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = "aws_instance.blog_id"
-          port = 80
-        }
-      }
     }
   }
 
